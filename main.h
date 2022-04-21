298df8315ad79d39e6ceb0c690f7d9fc5d11b5a1
Drewjoe <andrewjose711@gmail.com>
2022-04-21 03:48:03 +0100

    Header file

--- main.h
+++ main.h
@@ -8,7 +8,7 @@
 #include <limits.h>
 
 /**
- * struct specifier - struct specifier
+ * struct list - struct list
  * @va: valid character
  * @f: the associated functions.
  */
@@ -23,5 +23,6 @@ int print_c(va_list args);
 int print_percent(va_list args);
 int print_d(va_list args);
 int print_i(va_list args);
+int (*get_printf_func(char x))(va_list args);
 
 #endif
