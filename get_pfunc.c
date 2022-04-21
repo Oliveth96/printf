a6d0d2d20ac54aacc517e80dbe3d537c348a991c
Drewjoe <andrewjose711@gmail.com>
2022-04-21 04:15:32 +0100

    Gets array function

---
+++ get_pfunc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * get_pfunc - looks for list
+ * @x: function variable
+ * Return: function
+ */
+int (*get_pfunc(char x))(va_list args)
+{
+	int i = 0;
+	list_t arr[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"%", print_percent},
+		{"d", print_d},
+		{"i", print_i},
+		{"NULL, NULL"}
+	}
+	while (arr[i].va)
+	{
+		if (x == arr[i].va[0])
+			return (arr[i].f);
+		i++
+	}
+	return (NULL);
+}
