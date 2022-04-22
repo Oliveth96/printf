0ca0090a3d1568e6f4e5b176946d1b4dc7ba1263
Drewjoe <99388202+Drewjoe@users.noreply.github.com>
2022-04-22 11:16:38 +0100

    Print a output format

---
+++ printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _printf(const char *format, ...)
+{
+        if (format != NULL)
+        {
+                int count = 0, i;
+                int (*t)(va_list);
+                va_list args;
+
+                va_start(args, format);
+                i = 0;
+                if (format[0] == '%' && format[1] == '0')
+                        return (-1);
+                while (format != NULL && format[i] != '\0')
+                {
+                        if (format[i] == '%')
+                        {
+                                if (format[i + 1] == '%')
+                                {
+                                        count += _putchar(format[i]);
+                                        i += 2;
+                                }
+                                else
+                                {
+                                        t = get_pfunc(format[i + 1]);
+                                        if (t)
+                                                count += t(args);
+                                        else
+						count = _putchar(format[i]) + _putchar(format[i + 1]);
+                                        i += 2;
+                                }
+                        }
+                        else
+                        {
+                                count += _putchar(format[i]);
+                                i++;
+			}
+                }
+                va_end(args);
+                return (count);
+        }
+        return (-1);
+}
